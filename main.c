71e46079b2bd5059359ab9a5171627167796c3cf
Caroline <carolinebastiaa@gmail.com>
2025-02-16 23:31:18 -0800

    Structure created for patient and array of structures created, added option to continue with another selection after completing the first.

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include "patient.c"
 
 int main(void) {
-    
+    struct Patient patientList[50];
+    int count = 0;
+    int flag = 1;
+    char again;
+
+  while (flag == 1 )
+  {
+
     printf("\n1. Add Patient Record\n"
            "2. View All Patients\n"
            "3. Search Patient by ID\n"
@@ -10,16 +18,21 @@ int main(void) {
            "6. Exit");
 
     int selection;
+    printf("\nEnter your selection : ");
+    scanf("%d", &selection);
 
     switch (selection)
     {
         case 1:
             printf("You have chosen 'Add Patient Record'.");
-            // run method to add patient from patient class
+            struct Patient newPatient;
+            AddNewPatient();
+            addElement(newPatient, patientList);
+            printf("\nPatient record added successfully.");
             break;
         case 2:
             printf("You have chosen 'View All Patients'.");
-            // run method to print array list from patient class
+            printArray(count, patientList);
             break;
         case 3:
             printf("You have chosen 'Search Patient by ID'.");
@@ -41,5 +54,18 @@ int main(void) {
             printf("You have chosen 'Invalid Choice'. Please input a valid selection.");
             main();
     }
+
+      printf("\nWould you like to continue? y/n\n");
+      getchar();
+      scanf("%c", &again);
+      if (again == 'y'|| again == 'Y') {
+          flag = 1;
+      }
+      else
+      {
+          flag = 0;
+          printf("\nThank you, the program will now close.");
+      }
+  }
     return 0;
 }
---
+++ patient.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+ int patientID = 0;
+
+
+
+
+struct Patient {
+    int patientID;
+    char name[100];
+    int age;
+    char diagnosis[1000];
+    int roomNumber;
+};
+
+struct Patient AddNewPatient() {
+    patientID++;
+    struct Patient newPatient;
+    newPatient.patientID = patientID;
+    printf("\nPatient ID: %d", patientID);
+    getchar();
+    printf("\nPlease enter patients full name: ");
+    fgets(newPatient.name, sizeof(newPatient.name), stdin);
+    printf("Please enter patient's age: ");
+    scanf("%d", &newPatient.age);
+    getchar();
+    printf("Please enter patient's diagnosis name or reason for admission: ");
+    fgets(newPatient.diagnosis, sizeof(newPatient.diagnosis), stdin);
+    printf("Please enter patient's room number: ");
+    scanf("%d", &newPatient.roomNumber);
+
+    return newPatient;
+}
+void addElement(struct Patient newPatient, struct Patient patientList[])
+{
+    static int count = 0; // Keep track of the number of patients added
+    if (count < 50) { // Check if there's space in the array
+        patientList[count] = newPatient; // Add the new patient to the array
+        count++;
+    } else {
+        printf("Patient list is full!\n");
+    }
+}
+
+void printArray(int count, struct Patient patientList[])
+{
+    printf("\nCurrent Patient Records: ");
+    for (int i = 0; i < count; i++)
+    {
+        printf("\n%d. Patient ID: %d"
+               "\nName: %s"
+               "\nAge: %d"
+               "\nDiagnosis: "
+               "\nRoom Number: ", i + 1, patientList[i].patientID,
+               patientList[i].name, patientList[i].age,
+               patientList[i].diagnosis, patientList[i].roomNumber);
+    }
+}
+
